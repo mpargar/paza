f3af768d1cf7584714ff46bf407f3d10b7119bf6
e1016 <e10169610@gmail.com>
2018-09-27 17:45:43 -0600

    index progress

--- public/home.php
+++ public/home.php
@@ -1,7 +1,59 @@
 <?php include 'basics/header.php' ?>
-<main>
+<main class="main">
 
+  <section class="banner-container"></section>
 
+  <div class="main-wrapper">
+
+    <div class="row">
+      <div class="hatted-text">
+        <img src="/R/img/main/1.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+
+      <div class="hatted-text">
+        <img src="/R/img/main/2.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="hatted-text">
+        <img src="/R/img/main/1.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+
+      <div class="hatted-text">
+        <img src="/R/img/main/2.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="hatted-text">
+        <img src="/R/img/main/1.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+
+      <div class="hatted-text">
+        <img src="/R/img/main/2.png" alt="ícono sobre el texto">
+        <br>
+        <p>COMPRA O INVIERTE EN UN BIEN INMUEBLE</p>
+        <a href="#">MAS INFORMACIÓN  →</a>
+      </div>
+    </div>
+
+  </div>
 
 </main>
 <?php include 'basics/footer.php' ?>
