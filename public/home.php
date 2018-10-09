ee2e35f9befc27f9a386e9b32ebecc582c07a124
Miguel Parra García <miguel.parra0542@gmail.com>
2018-10-09 15:25:02 -0600

    De header a conf y banner

--- public/home.php
+++ public/home.php
@@ -3,7 +3,28 @@
 ?>
 <main>
     <!-- <BANNER>  -->
-
+    <div class="banner">
+    </div>
     <!-- </BANNER>  -->
+    <!-- <FastLinks> -->
+    <div class="fastList">
+        <ul>
+            <li>
+                <img src="/R/img/figure1.png">
+                <h3>TEXT TEST</h3>
+                <button>
+                    Mas información
+                </button>
+            </li>
+            <li>
+                <img src="/R/img/figure1.png">
+                <h3>TEXT TEST</h3>
+                <button>
+                    Mas información
+                </button>
+            </li>
+        </ul>
+    </div>
+    <!-- </FastLinks> -->
 </main>
 <?php include 'basics/footer.php' ?>
