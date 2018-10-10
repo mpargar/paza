2a470d64b4da5207385ad823d56c1d09490c985c
Miguel Parra García <miguel.parra0542@gmail.com>
2018-10-09 18:00:31 -0600

    LANG

--- public/buy.php
+++ public/buy.php
@@ -1,5 +1,6 @@
 <?php include 'basics/header.php' ?>
-<main class="buy-main">
+<main class="buy-main textPage">
   <h1><?= BUY_H1_TITLE ?></h1>
+  <?= BUY_TXT1 ?>
 </main>
 <?php include 'basics/footer.php' ?>
--- resources/lang/es_lang.php
+++ resources/lang/es_lang.php
@@ -25,7 +25,44 @@ define('CONTACT_H1_TITLE', 'Contacto');
 define('CONTACT_P_ONE', 'Puede dejar un mensaje en el siguiente formulario, le atenderemos en la brevedad');
 
 // buy
-define('BUY_H1_TITLE', 'Compra');
+define('BUY_H1_TITLE', 'Compra o invierte en un bien inmueble');
+define('BUY_TXT1', '
+    <p>Recomendaciones antes de comprar una casa, departamento o un terreno:</p>
+    
+    <p><b>Analiza tus finanzas:</b> El primer paso que debes dar es verificar si dispones del suficiente
+    presupuesto para cubrir los gastos que esto conlleva, tales como el depósito inicial y el
+    enganche, costos por apertura de crédito, gastos de escrituración y las mensualidades que
+    deberás cubrir durante el periodo que dure tu crédito hipotecario, entre otros gastos
+    como mudanza, decoración y contratación de servicios.</p>
+    
+    <p><b>Busca financiamiento de diversas instituciones:</b> Analiza minuciosamente varias
+    propuestas antes de elegir un crédito hipotecario. Compara las tasas e intereses que
+    ofrecen, así como la reputación y servicio de las instituciones financieras. Existen diversas
+    opciones de crédito que te pueden ayudar a comparar te recomendamos que lo conslites
+    con tu Asesor Inmobiliario.</p>
+
+    <p><b>Define la ubicación:</b> Existen diversos factores que determinan si conviene o no una casa
+    en una zona determinada, por ejemplo; cercanía con los centros de trabajo, escuelas y
+    hospitales, si hay espacios públicos de recreación, tiendas de autoservicios, rutas de
+    transporte, zonas de riesgo, etc.</p>
+
+    <p><b>Determina el diseño:</b> Lo ideal es analizar las características de la vivienda que estás por
+    comprar, como metros cuadrados, número de recámaras, baños, en qué nivel del edifico
+    se encuentra, etc. Además, es importante verificar si cuenta con estacionamiento, patio de
+    servicio, clósets o cocina integral, así como investigar si se puede ampliar o no, entre otros
+    puntos que deberás definir de acuerdo al número de integrantes de tu familia.</p>
+
+    <h3>Compara diversas opciones:</h3>
+    <p>Apóyate con tu agente inmobiliarios en donde puedes
+    comparar y las diversas opciones de casas y/o departamentos y programa una cita para
+    conocerlos físicamente.</p>
+
+    <p><b>Apóyate en los profesionales inmobiliarios:</b> Cuando se trata de vivienda en preventa,
+    nuevas o usada, ellos podrán apoyarte a conocer las condiciones del inmueble, como los
+    antecedentes de la propiedad, situación legal y revisar que toda la documentación y
+    servicios estén en orden.</p>
+  ');
+
 
 // sell
 define('SELL_H1_TITLE', 'Vende');
