77312e0900d88d5466165b3c9ebc2663943eba24
mpargar <miguel.parra0542@gmail.com>
2018-12-18 06:40:47 -0700

    Contact design fix

--- public/basics/contact-main.php
+++ public/basics/contact-main.php
@@ -1,5 +1,5 @@
 
-<div class="row-flex">
+
   <div class="column-small">
     <p class="c__purple">
       <a href="tel:+526121234625"><?=CONTACT_FORM_PHONE?>: +52.612.123.4625</a>, <a href="tel:+526121390675"><?=CONTACT_FORM_CELL?>: +52.612.139.0675</a> <?=O?> <a href="mailto:info@pazarealestate.com">e-mail: info@pazarealestate.com</a>
@@ -15,4 +15,3 @@
     <button id="send-contact-form">ENVIAR</button>
 
   </div>
-</div>
--- public/contact.php
+++ public/contact.php
@@ -4,8 +4,12 @@
 <main class="contact-main">
   
   <h1><?= CONTACT_H1_TITLE ?></h1>
-
-  <?php require('basics/contact-main.php') ?>
+  <div class="row-flex">
+    <?php require('basics/contact-main.php') ?>
+    <div style="height: auto;" class="column-big">
+        <img src="/R/img/contact.jpeg">
+    </div>
+  </div>
 
 </main>
 <?php include 'basics/footer.php' ?>
