76c80035f3fca7b49c57b4d606fea65e981977a4
mpargar <miguel.parra0542@gmail.com>
2019-06-03 01:35:59 -0600

    Agregar imagenes de la seccion VISTAMAR

--- public/vistamar.php
+++ public/vistamar.php
@@ -8,13 +8,37 @@
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="/R/img/cortez/vistamar/01.jpeg" alt="First slide">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/01.jpeg" alt="01 - slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="/R/img/cortez/vistamar/02.jpg" alt="Second slide">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/02.jpg" alt="02 - slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="/R/img/cortez/vistamar/03.jpg" alt="Third slide">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/03.jpg" alt="03 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/04.jpeg" alt="04 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/05.jpeg" alt="05 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/06.jpeg" alt="06 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/07.jpeg" alt="07 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/08.jpeg" alt="08 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/09.jpeg" alt="09 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/10.jpeg" alt="10 - slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="/R/img/cortez/vistamar/11.jpeg" alt="11 - slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -28,9 +52,17 @@
             </div>
             <!-- Slider -->
             <ul class="thumbnails">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0"><img class="d-block w-100" src="/R/img/cortez/vistamar/01.jpeg" alt="First slide"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"><img class="d-block w-100" src="/R/img/cortez/vistamar/02.jpg" alt="Second slide"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"><img class="d-block w-100" src="/R/img/cortez/vistamar/03.jpg" alt="Third slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="0"><img class="d-block w-100" src="/R/img/cortez/vistamar/01.jpeg" alt="01 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"><img class="d-block w-100" src="/R/img/cortez/vistamar/02.jpg" alt="02 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"><img class="d-block w-100" src="/R/img/cortez/vistamar/03.jpg" alt="03 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"><img class="d-block w-100" src="/R/img/cortez/vistamar/04.jpeg" alt="04 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"><img class="d-block w-100" src="/R/img/cortez/vistamar/05.jpeg" alt="05 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"><img class="d-block w-100" src="/R/img/cortez/vistamar/06.jpeg" alt="06 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"><img class="d-block w-100" src="/R/img/cortez/vistamar/07.jpeg" alt="07 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"><img class="d-block w-100" src="/R/img/cortez/vistamar/08.jpeg" alt="08 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"><img class="d-block w-100" src="/R/img/cortez/vistamar/09.jpeg" alt="09 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="9"><img class="d-block w-100" src="/R/img/cortez/vistamar/10.jpeg" alt="10 - slide"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="10"><img class="d-block w-100" src="/R/img/cortez/vistamar/11.jpeg" alt="11 - slide"></li>
             </ul>
             <p class="house-txt"><?=PUERTA_TEXT_2?></p>
         </li> 
