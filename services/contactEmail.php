6a3c14b8e14bb7f3c68b6a3f79eaa22ab7470ef0
Miguel Parra Garcia <miguelparradev@gmail.com>
2019-09-05 16:01:50 -0600

    New from email

--- services/contactEmail.php
+++ services/contactEmail.php
@@ -1,9 +1,9 @@
 <?php
-    $to      = 'info@pazarealestate.com, pabloglez40@gmail.com';
+    $to      = 'info@pazarealestate.com, pabloglez40@gmail.com, mariely@pazarealestate.com';
     $subject = 'Formulario de contacto';
     $message = "Name: " . $_POST['name'] . "\r\n" . "e-mail: " . $_POST['email'] . "\r\n" . "Phone: " . $_POST['phone'] . "\r\n\r\n" . $_POST['message'];
-    $headers = 'From: no- reply@pazarealestate.com' . "\r\n" .
-        'Reply-To: no-reply@pazarealestate.com' . "\r\n" .
+    $headers = 'From: pgz30mrnrl93@pazarealestate.com' . "\r\n" .
+        'Reply-To: pgz30mrnrl93@pazarealestate.com' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
     mail($to, $subject, $message, $headers);
 ?>
